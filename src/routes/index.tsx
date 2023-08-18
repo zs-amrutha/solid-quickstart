21c001a7cedeaf44fbced05a019921f2b4e3ba57
Amrutha <amrutha@zelarsoft.com>
2023-08-18 12:53:15 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
+{/*       <Title>Hello World</Title> */}
       <h1>Hello world!</h1>
 {/*       <Counter />
       <p>
